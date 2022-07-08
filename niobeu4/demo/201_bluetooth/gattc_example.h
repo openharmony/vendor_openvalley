8a587c528cf715f519611a56b8452b2567c5c0ad
xiezhipeng <xiezhipeng@talkweb.com.cn>
2022-07-08 16:19:10 +0800

    add: 添加蓝牙用例
    Signed-off-by: xiezhipeng <xiezhipeng@talkweb.com.cn>
    Change-Id: I4488dc5eb15bceceec62ef1764e29832ddadf379

---
+++ niobeu4/demo/201_bluetooth/gattc_example.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2022 Hunan OpenValley Digital Industry Development Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __GATTC_EXAMPLE_H__
+#define __GATTC_EXAMPLE_H__
+#include "stdbool.h"
+#include "ohos_bt_def.h"
+#include "ohos_bt_gatt.h"
+#include "bluetooth_device.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#define REMOTE_SERVICE_UUID 0x00FF
+#define REMOTE_NOTIFY_CHAR_UUID 0xFF01
+#define PROFILE_NUM 1
+#define PROFILE_A_APP_ID 0
+#define INVALID_HANDLE 0
+
+struct GattcProfileInst {
+    GattcBleCallback gattc_cb;
+    uint16_t gattc_if;
+    uint16_t app_id;
+    uint16_t conn_id;
+    uint16_t service_start_handle;
+    uint16_t service_end_handle;
+    uint16_t char_handle;
+    BdAddrs remote_bda;
+};
+#endif /* __GATTC_EXAMPLE_H__ */
